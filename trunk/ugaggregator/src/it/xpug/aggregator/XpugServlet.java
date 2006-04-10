432f31b59182bfdf6f6a377a8cbf9a48a7fc293a
matteov <matteov@13563377-7409-0410-a12c-8d51ca30d235>
2006-04-10 11:11:28 +0000

    Ripristinati i sorgenti dopo un piccolo casino che ho fatto

---
+++ trunk/ugaggregator/src/it/xpug/aggregator/XpugServlet.java
@@ -0,0 +1,32 @@
+package it.xpug.aggregator;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XpugServlet extends HttpServlet 
+{
+ 
+	public void doGet(HttpServletRequest req, HttpServletResponse rsp)
+			throws ServletException, IOException {
+		// ... handle GET requests
+		rsp.setContentType("text/html");
+		PrintWriter out = rsp.getWriter();
+		out.println("<html>");
+		out.println("<head><title>Servlet</title></head>");
+		out.println("<body>");
+		out.println("<p>This is a simple Servlet!</p>");
+		out.println("</body></html>");
+
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse rsp)
+			throws ServletException, IOException {
+		doGet(req,rsp);
+	}
+	
+}
